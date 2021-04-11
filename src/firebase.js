7397a5dfa22527b7fb93dcf38623b38b39658ad7
PuruDeveloper <mpurushottam27@gmail.com>
2021-04-11 22:06:03 +0530

    Adding website link

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = firebase.initializeApp ({
+    apiKey: "AIzaSyByLqrH6oZTOX5ZKfGltlWpZIBc3DXEMvI",
+  authDomain: "ibid-purushottam.firebaseapp.com",
+  projectId: "ibid-purushottam",
+  storageBucket: "ibid-purushottam.appspot.com",
+  messagingSenderId: "408859592519",
+  appId: "1:408859592519:web:77ba8c3b40780a14482e62",
+  measurementId: "G-NRMVDHQ1GH"
+});
+
+
